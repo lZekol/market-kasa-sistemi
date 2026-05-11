39fa4b824127de0ae470b710a856fe0e18958837
ataberkergin <ataberkergin22@gmail.com>
2026-05-11 20:33:44 +0300

    Update Odeme.java

--- Odeme.java
+++ Odeme.java
@@ -1,1 +1,21 @@
+/**
+ * Ödeme yöntemlerinin uyması gereken sözleşme.
+ */
+public interface Odeme {
+    /**
+     * Ödemeyi gerçekleştirir.
+     * @param tutar ödenecek miktar
+     * @return işlem başarılıysa true
+     */
+    boolean odemeYap(double tutar);
 
+    /**
+     * Ödeme yöntemi adı (makbuz için)
+     */
+    String odemeYontemi();
+
+    /**
+     * Kısa özet bilgi
+     */
+    String ozet();
+}
